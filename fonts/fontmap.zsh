5bbc663cd71c7e42227b3c81f67b0df82931a908
Stefan Stölzle <stefan@stoelzle.me>
2015-08-15 18:36:45 +1000

    fonts && theme

---
+++ fonts/fontmap.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# clear
+#
+# source ../fonts/octicons-regular.zsh
+#
+# for font in $(env | grep "CODEPOINT"); do
+#     # print -P "$font"
+#
+#     local var=${font[(ws:=:)0]}
+#     local symbol=${font[(ws:=:)2]}
+#
+#     print -P "\u${symbol} \t %F{cyan}$symbol$f \t %F{8}$var%f\n"
+# done
