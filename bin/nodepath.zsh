b7c81a907c4b57c7ca3823a6247171adbbb15d00
Stefan Stölzle <stefan@stoelzle.me>
2015-08-24 15:19:38 +1000

    Reformat vraiables

--- bin/nodepath.zsh
+++ bin/nodepath.zsh
@@ -5,21 +5,21 @@ source "../inc/helpers.zsh"
 source "$(brew --prefix nvm)/nvm.sh"
 
 # get default version
-FILE="${HOME}/.nvm/alias/default"
-VERSION="v$(cat ${FILE})"
+FILE="$HOME/.nvm/alias/default"
+VERSION="v$(cat $FILE)"
 SRC=$(nvm which $VERSION)
-DEST="${HOME}/bin/node"
+DEST="$HOME/bin/node"
 
-formatexec "$SRC"
+# formatexec "$SRC"
 
 # define SOURCE and TARGET
 if [ -f "$SRC" ]; then
     # cleanup old symlink
-    [ -f ${DEST} ] && formatexec "rm ${DEST}"
+    [ -f $DEST ] && formatexec "rm $DEST"
 
     # link it
-    formatexec "ln -s ${SRC} ${DEST}"
-    ok "linked %F{11}${SRC}%f to %F{2}${DEST}%f"
+    formatexec "ln -s $SRC $DEST"
+    ok "linked %F{11}$SRC%f to %F{2}$DEST%f"
 fi
 
 # DONE & RELOAD ----------------------------------------------------------------
