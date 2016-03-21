0207f618f42db8ac7c3216057a0cf2101047b60a
Stefan Stölzle <stoe@github.com>
2016-03-21 11:37:59 +0100

    Update brewup script

--- bin/brewup.zsh
+++ bin/brewup.zsh
@@ -1,8 +1,38 @@
 #!/bin/zsh
 
 brew=$(which brew)
-logger=/usr/bin/logger
 
-$brew update 2>&1  | $logger -t brewup.update
-$brew upgrade 2>&1 | $logger -t brewup.upgrade
-$brew cleanup 2>&1 | $logger -t brewup.cleanup
+if [ "$1" = "-h" ]; then
+  echo "Colorful Homebrew update script"
+  echo
+  echo "USAGE: update [-y]"
+  echo
+  echo "   -y  skip questions"
+  echo "   -h  display this help"
+  echo
+  exit 0
+fi
+
+echo "\033[93mFetching packages list:\033[0m"
+$brew update
+brewsy=`$brew outdated | wc -l | awk {'print $1'}`
+
+if [ $brewsy != 0 ]; then
+  echo "\033[93mOutdated packages:\033[0m" $brewsy
+  echo
+  $brew outdated
+  echo
+
+  if [ "$1" != "-y" ]; then
+    ask=$(echo "\033[93mUpdate the these packages?\033[92m [yn] \033[0m")
+    read -p "$ask" yn
+  fi
+
+  if [ "$yn" = "y" ]; then
+    $brew upgrade --all && $brew cleanup
+  else
+    echo "\033[93mOK, not doing anything\033[0m\n"
+  fi
+else
+  echo "\033[93mNothing to do\033[0m"
+fi
