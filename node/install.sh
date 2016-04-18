8be4fa76cf9ff6527835a79b068e0c7b9d75431f
Stefan Stölzle <stefan@stoelzle.me>
2016-04-18 10:29:41 +0200

    Update node.js install script

--- node/install.sh
+++ node/install.sh
@@ -3,18 +3,18 @@
 nvm="${HOME}/.nvm"
 
 [ ! -d "${nvm}" ] && mkdir "${nvm}"
-[ ! -f $HOME/.npmrc ] && touch "$HOME/.npmrc" # npm config file
+[ ! -f "$HOME/.npmrc" ] && touch "$HOME/.npmrc" # npm config file
 export NVM_DIR="${nvm}"
 source "$(brew --prefix nvm)/nvm.sh"
 
-printf "\r[ \033[00;34mnode version\033[0m ]\n"
+printf "\rinstalling \033[00;34mnode version\033[0m\n"
 nvm install iojs  # install latest io.js version
 nvm install node  # install latest node.js version
 nvm alias default node
 nvm use default
 
 # install dependencies
-printf "\n\r[ \033[00;34mnode modules\033[0m ]\n"
+printf "\n\rinstalling \033[00;34mnode modules\033[0m\n"
 MODS=(
     cordova phonegap ios-sim
     eslint babel babel-cli babel-eslint
@@ -31,13 +31,13 @@ MODS=(
 )
 
 for MOD in $MODS; do
-    if $(npm -g ls | grep "$MOD@[0-9\.]*$" > /dev/null 2>&1); then
-        npm upgrade -g "$MOD"
-        printf "upgraded \033[00;34m$MOD\033[0m\n"
-    else
-        npm install -g "$MOD" > /dev/null 2>&1
-        printf  "installed \033[00;34m$MOD\033[0m\n"
-    fi
+  if $(npm -g ls | grep "$MOD@[0-9\.]*$" > /dev/null 2>&1); then
+      npm upgrade -g "$MOD"
+      printf "└ upgraded \033[00;32m%s\033[0m\n" "$MOD"
+  else
+      npm install -g "$MOD" > /dev/null 2>&1
+      printf  "└ installed \033[00;32m%s\033[0m\n" "$MOD"
+  fi
 done
 
 npm ls -g | grep "^[└├]" | sed "s/─┬/──/g"
