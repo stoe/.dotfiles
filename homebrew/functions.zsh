0149bc655a100c3dcb0b8e4a8192642cd9a7a55a
Stefan Stölzle <stefan@stoelzle.me>
2016-04-18 13:13:35 +0200

    Update brew files

--- homebrew/functions.zsh
+++ homebrew/functions.zsh
@@ -1,32 +1,32 @@
-  #!/bin/zsh
+#!/bin/zsh
 
-source "$ZSH/inc/helpers.sh"
-
-brewup() {
+function brewup() {
   # see https://gist.github.com/fvdm/1715d580a22503ce115c#file-homebrew_update-sh
   # thanks https://github.com/fvdm
 
-  brew=$(which brew)
+  local _brew=$(which brew)
 
-  if [ "$1" = "-h" ]; then
-    section "Colorful Homebrew update script"
-    echo
-    echo "USAGE: brewup [-y]"
-    echo
-    echo "   -y  skip questions"
-    echo "   -h  display this help"
-    echo
-    exit 0
-  fi
+  # if [ "$1" = "-h" ]; then
+  #   section "Colorful Homebrew update script"
+  #   echo
+  #   echo "USAGE: brewup [-y]"
+  #   echo
+  #   echo "   -y  skip questions"
+  #   echo "   -h  display this help"
+  #   echo
+  #   exit 0
+  # fi
+
+  echo $_brew
 
   section "Fetching packages list"
-  $brew update
-  brewsy=$($brew outdated | wc -l | awk '{print $1}')
+  formatexec "$_brew update"
+  local _brewsy=$($_brew outdated | wc -l | awk '{print $1}')
 
-  if [ "$brewsy" != 0 ]; then
-    print -P "%F{3}Outdated packages:%f" "$brewsy"
+  if [ "$_brewsy" != 0 ]; then
+    print -P "%F{3}Outdated packages:%f" "$_brewsy"
     echo
-    $brew outdated
+    formatexec "$_brew outdated"
 
     if [ "$1" != "-y" ]; then
       question "Update the these packages?" "yn"
@@ -35,7 +35,7 @@ brewup() {
     fi
 
     if [ "$ask" = "y" ]; then
-      $brew upgrade --all
+      formatexec "$_brew upgrade --all"
     else
       ok "OK, not doing anything"
     fi
@@ -44,8 +44,8 @@ brewup() {
   fi
 
   section "Doctor & Cleanup"
-  $brew doctor
-  $brew cleanup
+  formatexec "$_brew doctor"
+  formatexec "$_brew cleanup"
 
   echo
   ok "DONE"
