461ec30f4621e91e1dcd593015351f998f99cfc7
Stefan Stölzle <stefan@stoelzle.me>
2016-05-17 14:20:53 +0200

    Set computer name 0x73746f65

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -13,10 +13,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "0x73746f652d6d6163"
-sudo scutil --set HostName "0x73746f652d6d6163"
-sudo scutil --set LocalHostName "0x73746f652d6d6163"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x73746f652d6d6163"
+# sudo scutil --set ComputerName "0x73746f65"
+# sudo scutil --set HostName "0x73746f65"
+# sudo scutil --set LocalHostName "0x73746f65"
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x73746f65"
 
 # Set standby delay to 24 hours (default is 1 hour)
 sudo pmset -a standbydelay 86400
