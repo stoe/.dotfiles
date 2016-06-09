1c1812272d3e7033ec304dd9932fe298f4f13198
Stefan Stölzle <stefan@stoelzle.me>
2016-06-09 15:04:07 +0200

    Move ls aliases to end of .zshrc

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,14 +1,3 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-# if $(gls &>/dev/null)
-# then
-#   alias l="gls -lAh --color"
-#   alias ll="gls -l --color"
-#   alias la='gls -A --color'
-#   alias ls="gls -F --color"
-# fi
-
 # DOS style clear
 alias cls="clear"
 
