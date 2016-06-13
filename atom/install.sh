b0d6749ca561de46cbef854205e7a836daf3faa2
Stefan Stölzle <stefan@stoelzle.me>
2016-06-13 18:53:32 -0400

    Add atom.io install script

---
+++ atom/install.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+source "$HOME/.dotfiles/inc/helpers.sh"
+
+section "atom packages"
+PACKAGES=(
+  atom-linkwrap
+  atom-material-syntax
+  atom-material-syntax-light
+  atom-material-ui
+  atomic-chrome
+  autocomplete-emojis
+  date
+  editorconfig
+  gist-it
+  git-plus
+  language-apache
+  language-ini
+  language-nginx
+  markdown-table-formatter
+  merge-conflicts
+  minimap
+  minimap-git-diff
+  project-manager
+)
+
+for PACKAGE in $PACKAGES; do
+  if $(apm ls | grep "$PACKAGE@[0-9\.]*$" > /dev/null 2>&1); then
+      formatexec "apm upgrade $PACKAGE"
+      print -P "  └ %F{3}upgraded%f"
+  else
+      formatexec "apm install $PACKAGE > /dev/null 2>&1"
+      print -P "  └ %F{2}installed%f"
+  fi
+done
