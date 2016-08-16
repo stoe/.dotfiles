6eba723caa5211e0c2b60dfe5173c607cafc6e3f
Stefan Stölzle <stefan@stoelzle.me>
2016-08-16 15:13:29 +0200

    Move cleanup from alias to function

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,8 +4,5 @@ alias cls="clear"
 # flush DNS cache
 alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;"
 
-# delete thos .DS_Store files in style
-alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
-
 # empty all trashes
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
