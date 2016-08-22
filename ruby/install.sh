b5bbbbdca3ef8d799e0f2c89e353ad86377a7324
Stefan Stölzle <stefan@stoelzle.me>
2016-08-22 15:14:58 +0200

    Add global gems

---
+++ ruby/install.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+source "$HOME/.dotfiles/inc/helpers.sh"
+
+# install dependencies
+GEMS=(
+  bundler
+  pry
+)
+
+for GEM in $GEMS; do
+  if $(gem list --no-version | grep "$GEM" > /dev/null 2>&1); then
+    formatexec "gem update $GEM"
+    print -P "  └ %F{3}upgraded%f"
+  else
+    formatexec "gem install $GEM > /dev/null 2>&1"
+    print -P "  └ %F{2}installed%f"
+  fi
+  echo
+done
