92a3f55c9ebb0e7e5eda2c8bdaabcd873d126f33
Stefan Stölzle <stefan@stoelzle.me>
2017-01-26 14:44:16 +0100

    Add allup alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,3 +6,5 @@ alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;"
 
 # empty all trashes
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+
+alias allup="githubup; brewup; apm update"
