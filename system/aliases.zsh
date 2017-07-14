ed381673311744bdc6b81f5c73e0c932de464778
Stefan Stölzle <stefan@stoelzle.me>
2017-07-14 13:30:11 +0300

    Add aws aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,3 +9,8 @@ alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo 
 
 alias allup="githubup; brewup; apm update"
 alias allclean="emptytrash; cleanup; lsclean; lpclean; flushdns"
+
+
+# AWS
+alias aws-github.start="aws ec2 start-instances --instance-ids i-0d295c6866624bb6f i-0a8425c65d8d171a5"
+alias aws-github.stop="aws ec2 stop-instances --instance-ids i-0d295c6866624bb6f i-0a8425c65d8d171a5"
