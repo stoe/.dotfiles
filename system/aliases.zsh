b59adbd717498e10d6c238f32525185acb317802
Stefan Stölzle <stefan@stoelzle.me>
2017-09-27 08:48:11 +0200

    Rewrite cleanup
    inspired by https://github.com/Alhadis/Snippets/blob/master/shell/dsclean.sh

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,7 +8,7 @@ alias flushdns="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;"
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
 
 alias allup="githubup; brewup; apm update"
-alias allclean="emptytrash; cleanup; lsclean; lpclean; flushdns"
+alias allclean="emptytrash; cleanup -h; lsclean; lpclean; flushdns"
 
 # GitHub Professional Services PDFify
 alias ghpdf="pdfify --header ~/github/pdfify-node/assets/header.hbs --style ~/github/pdfify-node/assets/style.css"
