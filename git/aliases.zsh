97faa25f581e1c0482e2ea9081a0802fbe2e6437
Stefan Stölzle <stoe@github.com>
2018-07-07 20:05:01 +0200

    Clean out

--- git/aliases.zsh
+++
@@ -1,8 +0,0 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] )); then
-  alias git=$hub_path
-fi
-
-alias doctoc="doctoc --github"
--- system/keys.zsh
+++
@@ -1,4 +0,0 @@
-# Pipe my public key to my clipboard.
-alias pubkey-stoe="more ~/.ssh/id_rsa_stoe.pub | pbcopy | echo '=> stoe public key copied to pasteboard'"
-alias pubkey-git="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> GitHub public key copied to pasteboard'"
-alias pubkey="pubkey-stoe"
