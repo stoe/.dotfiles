5f4e6e4563546659f53abce65abb6bbb4920f4ee
Stefan Stölzle <stoe@github.com>
2018-07-08 19:20:58 +0200

    Rework all the things™

---
+++ inc/paths.zsh
@@ -0,0 +1,25 @@
+# user `bin/`
+export PATH="${HOME}/bin:${PATH}"
+
+# Node
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+
+# Ruby
+export PATH="${HOME}/.rbenv/bin:${PATH}"
+eval "$(rbenv init -)"
+
+# Go
+export PATH="/usr/local/opt/go/libexec/bin:${PATH}"
+export GOPATH=$(go env GOPATH)
+export PATH="${GOPATH}/bin:${PATH}"
+
+# Python
+export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
+
+# Travis CI
+# added by travis gem
+[ -f "${HOME}/.travis/travis.sh" ] && source "${HOME}/.travis/travis.sh"
+
+# GitHub Services Training
+test -f "${HOME}/.trainingmanualrc" && source "${HOME}/.trainingmanualrc"
