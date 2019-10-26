72c2c1942d8fec499a6b99aeb4142efd2e2c8fa1
Stefan Stölzle <stoe@github.com>
2019-10-26 18:15:25 +0200

    Use default colors

--- inc/helpers.zsh
+++ inc/helpers.zsh
@@ -1,18 +1,17 @@
 #!/bin/zsh
 
 abort() {
-  print -P "\n✘ %F{3}aborting%f $1\n"
+  print -P "\n✘ ${yellow}aborting${reset_color} $1\n"
   return
 }
 
 disabled() {
-  print -P "\n%F{1}disabled%f."
-  exit 0
+  print -P "\n${red}disabled${reset_color}."
+  return 1
 }
 
 finished() {
-  print -P "\nDONE."
-  print -P "\n%F{8}✎ with %F{1}♥%F{8} by %F{12}stoe%F{8} (https://github.com/stoe/.dotfiles)%f"
+  print -P "\n${grey}✎ with ${red}♥${grey} by %F{12}stoe${grey} (https://github.com/stoe/.dotfiles)${reset_color}"
 
   source "$HOME/.zshrc"
 }
@@ -20,22 +19,22 @@ finished() {
 formatexec() {
   local _exec="$1"
 
-  print -P "%F{8}> $_exec%f"
+  print -P "${grey}> $_exec${reset_color}"
   eval "$_exec"
 }
 
 ok() {
-  print -P "\n[ %F{2}✓%f ] $1\n"
+  print -P "\n[ ${green}✓${reset_color} ] $1\n"
 }
 
 question() {
   local question="$1"
   local options="$2"
 
-  print -P "%F{3}$question%f"
-  [ "$options" != "" ] && print -P "%F{2}[$options]%f"
+  print -P "${cyan}${question}${reset_color}"
+  [ "$options" != "" ] && print -P "${green}[$options]${reset_color}"
 }
 
 section () {
-  print -P "\n[ %F{4}$1%f ] $2"
+  print -P "\n[ ${magenta}$1${reset_color} ] $2"
 }
