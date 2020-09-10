b4ef54f608b41b3cc55de9b9ffee5181ccc3d0cc
Stefan Stölzle <stoe@github.com>
2020-09-10 08:04:04 +0200

    🐛 Fix PATH

--- inc/paths.zsh
+++ inc/paths.zsh
@@ -1,33 +1,36 @@
 # OpenSSL
-export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+export PATH="${PATH}:/usr/local/opt/openssl@1.1/bin"
 export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
 # bin
-export PATH="/usr/local/bin:${PATH}"
-export PATH="/usr/local/lib:${PATH}"
-export PATH="/usr/local/sbin:${PATH}"
-export PATH="${HOME}/bin:${PATH}"
+export PATH="${PATH}:/usr/local/bin"
+export PATH="${PATH}:/usr/local/lib"
+export PATH="${PATH}:/usr/local/sbin"
+export PATH="${PATH}:${HOME}/bin"
+
+# Node
+export NODE_ENV=development
 
 # Go
 export GOPATH="${HOME}/.go"
 export GOBIN="${GOPATH}/bin"
-
-export PATH="/usr/local/opt/go/libexec/bin:${PATH}"
-export PATH="${GOBIN}:${PATH}"
+export PATH="${PATH}:${GOBIN}"
+export GOROOT="$(brew --prefix)/opt/go/libexec"
+export PATH="${PATH}:${GOROOT}/bin"
 
 # Python
-export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
+export PATH="${PATH}:/usr/local/opt/python/libexec/bin"
 
 # Ruby
-export PATH="${HOME}/.rbenv/bin:${PATH}"
+export PATH="${PATH}:${HOME}/.rbenv/bin"
 eval "$(rbenv init -)"
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
 # MySQL
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+export PATH="${PATH}:$(brew --prefix)/opt/mysql@5.7/bin"
 
 # Google Cloud
 source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
