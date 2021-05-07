654982d3d27924cc26cc71168898018b1000f9fe
Stefan Stölzle <stoe@github.com>
2021-05-07 08:37:16 +0200

    🆙 Delete duplicate Ruby opts

--- inc/paths.zsh
+++ inc/paths.zsh
@@ -3,7 +3,6 @@ export PATH="${PATH}:/usr/local/opt/openssl@1.1/bin"
 export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
 # bin
 export PATH="${PATH}:/usr/local/bin"
