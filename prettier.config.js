2af6796bfb073913c7748d99fb60fe536baf6504
Stefan Stölzle <stefan@stoelzle.me>
2024-06-30 11:49:14 +0200

    💄 Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -3,8 +3,5 @@ import * as prettierGitHubConfig from '@github/prettier-config'
 export default {
   ...prettierGitHubConfig.default,
   // Add your overrides here
-  semi: false,
-  singleQuote: true,
   tabWidth: 2,
-  trailingComma: 'all',
 }
